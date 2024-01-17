b45d3482b8ae68c4e5b7266da66932dffd5519d7
MattiaPaiano91 <paianomattia1@gmail.com>
2024-01-17 15:44:35 +0100

    -Esercizio completato

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+export default {
+  data() {
+    return {
+      message:"Hello World! Questo dato è preso dall'app vue"
+    };
+  },
+  components: {
+    AppTitle
+  }
+
+}
+</script>
+
+<template>
+  <div>
+    <AppTitle/>
+    <main>
+      <h1>{{ message }}</h1>
+    </main>
+  </div>
+</template>
+ 
+
+<style scoped>
+  main{
+    width: 100%;
+    text-align: center;
+  }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,30 @@
+<script>
+  export default {
+    data() {
+      return {
+        message: "La mia prima app con Vite!"
+      };
+    },
+  }
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>{{ message }}</h1>
+    </header>
+  </div>
+ 
+</template>
+
+<style scoped>
+    header{
+      height: 200px;
+      width: 100%;
+      background-color: darkgray;
+      text-align: center;
+      line-height: 200px;
+    }
+</style>
+    
+
